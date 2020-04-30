d7ba202216e444121367314d63fdef74998c0583
Your Name <you@example.com>
2020-04-30 23:18:02 +0530

    some work left

--- Routes/index.js
+++ Routes/index.js
@@ -1,9 +1,11 @@
+const cors = require("cors");
 const express = require("express");
 const app = express();
 const router = require("./Controllers");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
-const cors = require("cors");
+
+app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
