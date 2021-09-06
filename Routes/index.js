353f63426a07c9d0c205ec10692c241fa246f6c2
NamanZelawat <namanzelawat@gmail.com>
2021-09-06 14:43:18 +0530

    CORS

--- Routes/index.js
+++ Routes/index.js
@@ -9,6 +9,9 @@ app.use(function (req, res, next) {
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "*");
 
   next();
 });
